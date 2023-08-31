490118a04317ddf696529f97fe5360f979d9da38
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-08-31 15:38:28 +0300

    solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,19 @@
 package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Book> books = new ArrayList<>();
+        books.add(new Book("Power of now", "Eckhart Tole", 50));
+        books.add(new Book("Who broke the chain...", "Neil Anderson", 65));
+
+        Discount discount = new Discount("Summar sale", 20);
+
+        books.stream().forEach(book -> {
+            double newPrice = book.price() - (book.price() * discount.percentage() / 100);
+            System.out.println("The Summer price for " + book.title() + " by " + book.author() + " is " + newPrice);
+        });
     }
 }
